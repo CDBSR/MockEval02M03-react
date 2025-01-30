5eb547c55d65b259854fd5e69715a392a11405e4
BrajsundariRathor <rathorbrajsundari@gmail.com>
2025-01-30 12:31:39 +0530

    first commit

---
+++ src/components/Hello.jsx
@@ -0,0 +1,8 @@
+
+export const Hello = () => {
+    return (
+        <div>
+            <h1>Hello, This is the demo for the deploying the react projcet into the Github Pages.</h1>
+        </div>
+    )
+}
